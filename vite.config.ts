9c0362cf00e1e5513e8b55d27b782bc12791aaf1
C-Junior <dkjunior13@gmail.com>
2026-02-08 03:20:01 -0300

    fix login test

--- vite.config.ts
+++ vite.config.ts
@@ -99,7 +99,8 @@ export default defineConfig({
     server: {
         headers: {
             // Allow Firebase popup authentication
-            'Cross-Origin-Opener-Policy': 'same-origin-allow-popups'
+            'Cross-Origin-Embedder-Policy': 'credentialless',
+            'Cross-Origin-Opener-Policy': 'same-origin'
         }
     }
 })
